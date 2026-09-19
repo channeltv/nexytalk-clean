17f6d7081fce33fd3a6a6da1e9b2b64bb6cbd6bb
channeltv <ustameta@gmail.com>
2026-09-20 01:36:38 +0200

    Update server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ app.post('/api/login', async (req, res) => {
     }
 
     const user = await User.findOne({
-        \$or: [{ email: credenziale }, { username: credenziale }]
+        $or: [{ email: credenziale }, { username: credenziale }]
     });
 
     if (!user || user.password !== password) {
